fb7718681add7994e233c31d365a3ee0a65f32d4
Ritesh Tailor <rdtailor@gmail.com>
2021-01-14 13:57:56 +0530

    Add functions to start/stop location tracking

--- permission-types/locationPermissions.js
+++ permission-types/locationPermissions.js
@@ -33,7 +33,7 @@ function asyncPromptGeoLocationPermission() {
 }
 
 /**
- * @method promptGeoLocationPermission
+ * @method getGeoLocationPermission
  * @param {*} successCallback
  * @param {*} errorCallback
  * @description This function is to get location permission state without prompting the user for location access and return result via callback.
@@ -53,8 +53,63 @@ function getGeoLocationPermission(successCallback, errorCallback) {
   }
 }
 
+/**
+ * @method startLocationTracking
+ * @param {Object} args
+ * @param {Function} args.successCallback
+ * @param {Function} args.errorCallback
+ * @param {Object} args.positionOptions
+ * @param {Number} args.thresholdTime
+ * @description This function is to continuously track user location. A threshold can be set by passing the miliseconds for thresholdTime parameter.
+ */
+function startLocationTracking({
+  successCallback,
+  errorCallback,
+  positionOptions,
+  thresholdTime,
+}) {
+  let enableCall = true;
+  const locationWatchPositionId = navigator.geolocation.watchPosition(
+    (position) => {
+      if (thresholdTime) {
+        if (!enableCall) {
+          return;
+        }
+        enableCall = false;
+      }
+      try {
+        successCallback(position);
+      } catch (err) {
+        errorCallback(err);
+      }
+      if (thresholdTime) {
+        setTimeout(() => {
+          enableCall = true;
+        }, thresholdTime);
+      }
+    },
+    (err) => {
+      navigator.geolocation.clearWatch(locationWatchPositionId);
+      errorCallback(err);
+    },
+    positionOptions
+  );
+  return locationWatchPositionId;
+}
+
+/**
+ * @method stopLocationTracking
+ * @param {Number} locationTrackingId
+ * @description This function is to stop tracking user location which was started before by passing locationTrackingId.
+ */
+function stopLocationTracking(locationTrackingId) {
+  navigator.geolocation.clearWatch(locationTrackingId);
+}
+
 exports.locationPermissions = {
   promptGeoLocationPermission,
   asyncPromptGeoLocationPermission,
   getGeoLocationPermission,
+  startLocationTracking,
+  stopLocationTracking,
 };
